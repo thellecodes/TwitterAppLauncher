adbef145760ecdee764e0f04821eff61765b811f
Samuel Anthony <boltcode17@gmail.com>
2020-06-19 16:43:15 -0700

    With splash screen

---
+++ .history/App_20200619160808.js
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        alignItems: 'center',
+        jusitifyContent: 'center',
+      }}>
+      <Text>WelcomeScreen</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ .history/App_20200619160827.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        alignItems: 'center',
+        jusitifyContent: 'center',
+      }}>
+      <Text>WelcomeScreen</Text>
+    </View>
+  );
+};
+
+export default App;
